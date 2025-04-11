224d10b0fbc22b2b526c362c905019ec19403a23
mikhb01 <143737949+mikhb01@users.noreply.github.com>
2025-04-11 16:38:30 +0900

    Added async functions to fetch the data from the json file to have it externally stored
    Still needs work for error catching

--- Website/JS/scripts.js
+++ Website/JS/scripts.js
@@ -1,34 +1,40 @@
 let i = 0;
 
-function session() {
-    let text = '{"words":[' +
-    '{"vocabJP":"学校", "vocabENG":"School"},' +
-    '{"vocabJP":"自動販売機", "vocabENG":"Vending machine"},' + 
-    '{"vocabJP":"会社", "vocabENG":"Company"}]}';
 
-    const obj = JSON.parse(text);
+async function loadWords() {
+    const response = await fetch("../JSON/test.json", {headers:{'Accept': 'application/json'}});
+
+    let text = await response.json();
+    let jsonString = JSON.stringify(text);
+
+    let obj = JSON.parse(jsonString);
+    console.log(obj);
 
     document.getElementById("wordKanji").innerHTML = obj.words[i].vocabJP;
-    document.getElementById("writingAnswer").style.visibility = "visible";
-    document.getElementById("startButton").style.visibility = "hidden";
-    document.getElementById("submitButton").style.visibility = "visible";
+    document.getElementById("writingAnswer").style.display = "initial";
+    document.getElementById("startButton").style.display = "none";
+    document.getElementById("submitButton").style.display = "initial";
+
+    
+
 }
 
-function next() {
-    let text = '{"words":[' +
-    '{"vocabJP":"学校", "vocabENG":"School"},' +
-    '{"vocabJP":"自動販売機", "vocabENG":"Vending machine"},' + 
-    '{"vocabJP":"会社", "vocabENG":"Company"}]}';
-    
-    const obj = JSON.parse(text);
+async function nextWord() {
+    const response = await fetch("../JSON/test.json", {headers:{'Accept': 'application/json'}});
+
+    let text = await response.json();
+    let jsonString = JSON.stringify(text);
+
+    let obj = JSON.parse(jsonString);
+    console.log(obj);
 
     let x = document.getElementById("writingAnswer").value;
-      document.getElementById("word").style.backgroundColor = "palevioletred";
-        if(x.toLowerCase() === obj.words[i].vocabENG.toLowerCase()){
-            i++;
-            document.getElementById("word").innerHTML = obj.words[i].vocabJP;
-        }else{
-            document.getElementById("word").style.backgroundColor = "#840a0a";
-            document.getElementById("word").innerHTML = obj.words[i].vocabENG;
-      }
+    document.getElementById("word").style.backgroundColor = "palevioletred";
+      if(x.toLowerCase() === obj.words[i].vocabENG.toLowerCase()){
+          i++;
+          document.getElementById("wordKanji").innerHTML = obj.words[i].vocabJP;
+      }else{
+          document.getElementById("word").style.backgroundColor = "#840a0a";
+          document.getElementById("word").innerHTML = obj.words[i].vocabENG;
+    }
 }
